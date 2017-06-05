a1a7d0e030e564f9e3c99d5be133eca56d55dcb2
jenitivecase <jbrussow@gmail.com>
2017-06-05 12:05:03 -0500

    added basic options and packages

--- base_simulation_script.R
+++ base_simulation_script.R
@@ -1,4 +1,12 @@
 #### SETUP ####
+options(scipen = 999)
+options(stringsAsFactors = FALSE)
+
+library(truncnorm)
+library(dplyr)
+library(tidyr)
+library(mirt)
+
 N_people <- 6000
 N_groups <- 300
 groupsize_min <- 5
