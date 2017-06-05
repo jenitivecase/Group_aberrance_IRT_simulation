9b72f9f905cfdef082560274297ef9bf04a992d8
jenitivecase <jbrussow@gmail.com>
2017-06-05 16:25:59 -0500

    simulation functions. need to vectorize; slow with 6000 respondents

--- base_simulation_script.R
+++ base_simulation_script.R
@@ -7,10 +7,13 @@ library(dplyr)
 library(tidyr)
 library(mirt)
 
+source("functions.R")
+
 N_people <- 6000
 N_groups <- 300
 groupsize_min <- 5
 groupsize_max <- 35
+group_sd <- 1
 n_items <- 60
 mean_increase <- 0.5
 yr_corr <- 0.6
@@ -20,8 +23,8 @@ n_cheat <- N_groups * 0.05
 items <- item_sim(n_items, b_mean = 0, b_sd = 1,
                   a_min = 0.5, a_max = 3)
 
-people <- ability_sim(N_people, theta_mean=0, theta_sd=1,
-                    N_groups, groupsize_min, groupsize_max, 
-                    mean_increase, yr_corr, cheat_eff)
+people <- two_yr_ability_sim(N_people, theta_mean=0, theta_sd=1,
+                             N_groups, groupsize_min, groupsize_max, group_sd,
+                             mean_increase, yr_corr, n_cheat, cheat_eff)
 
 dataset <- one_dataset(person_param = people, item_param = items)
