86d8747213bf73bc079c00ee42fa88256b819de0
jenitivecase <jbrussow@gmail.com>
2017-06-05 12:20:05 -0500

    basic simulation

--- base_simulation_script.R
+++ base_simulation_script.R
@@ -11,3 +11,12 @@ N_people <- 6000
 N_groups <- 300
 groupsize_min <- 5
 groupsize_max <- 35
+n_items <- 60
+
+items <- item_sim(n_items, b_mean = 0, b_sd = 1,
+                  a_min = 0.5, a_max = 3)
+
+people <- ability_sim(N_people, theta_mean=0, theta_sd=1,
+                    N_groups, groupsize_min, groupsize_max)
+
+dataset <- one_dataset(person_param = people, item_param = items)
