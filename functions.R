692dd46f057062904e8db3be68ce97b3591e7bb2
jenitivecase <jbrussow@gmail.com>
2017-06-05 12:03:50 -0500

    adapting sim functions to this project

--- functions.R
+++ functions.R
@@ -1,44 +1,32 @@
 #### DATA GENERATION ####
 
 #simulate a set of items
-item_sim <- function(n_items, n_DIF, b_mean, b_sd, a_min, a_max, 
-                     nodif_mean, nodif_sd, dif_mean, dif_sd){
-  item_param <- matrix(NA, nrow = n_items, ncol = 3)
-  colnames(item_param) <- c("b_param", "a_param", "dif_param")
+item_sim <- function(n_items, b_mean, b_sd, a_min, a_max, ){
+  item_param <- matrix(NA, nrow = n_items, ncol = 2)
+  colnames(item_param) <- c("b_param", "a_param")
   
   item_param[, "b_param"] <- rnorm(nrow(item_param), b_mean, b_sd)
   #change to draw uniform distribution .5, 3.5
   item_param[, "a_param"] <- runif(nrow(item_param), a_min, a_max)
   
-  if(n_DIF > 0){
-    noDIF_rows <- c(1:(nrow(item_param)-n_DIF))
-    DIF_rows <- c((nrow(item_param)-n_DIF+1):nrow(item_param))
-    item_param[noDIF_rows, "dif_param"] <- rnorm(length(noDIF_rows), 
-                                                 nodif_mean, nodif_sd)
-    item_param[DIF_rows, "dif_param"] <- rnorm(length(DIF_rows), dif_mean, dif_sd)
-  } else {
-    item_param[, "dif_param"] <- rnorm(nrow(item_param), nodif_mean, nodif_sd)
-  }
-  
   return(item_param)
 }
 
 #simulate a set of people's ability scores
-ability_sim <- function(N_people, P_REF, ref_theta_mean, ref_theta_sd, 
-                        focal_theta_mean, focal_theta_sd){
-  ability_scores <- matrix(NA, nrow = N_people, ncol = 2)
-  colnames(ability_scores) <- c("theta", "group")
-  ref_cutoff <- nrow(ability_scores)*P_REF
-  ref_rows <- c(1:ref_cutoff)
-  focal_rows <- c((ref_cutoff+1):nrow(ability_scores))
+ability_sim <- function(N_people, theta_mean, theta_sd,
+                        N_groups, groupsize_min, groupsize_max){
+  ability_scores <- matrix(NA, nrow = N_people, ncol = 3)
+  colnames(ability_scores) <- c("theta", "group1", "group2")
   
-  ability_scores[ref_rows, "theta"] <- rnorm(length(ref_rows), 
-                                             ref_theta_mean, ref_theta_sd)
-  ability_scores[ref_rows, "group"] <- 0
+  ability_scores[,"theta"] <- rnorm(N_people, theta_mean, theta_sd)
   
-  ability_scores[focal_rows, "theta"] <- rnorm(length(focal_rows), 
-                                               focal_theta_mean, focal_theta_sd)
-  ability_scores[focal_rows, "group"] <- 1
+  group_init <- round(rtruncnorm(n = N_groups, a = groupsize_min, 
+                                 b = groupsize_max, 
+                                 mean = 20, sd = 5))
+  
+  ability_scores[,"group1"] <- group_init
+
+  ability_scores[,"group2"] <- group_init[sample(length(group_init), length(group_init))]
   
   return(ability_scores)
 }
