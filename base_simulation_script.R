bed10cf55a1777d5999e86e561c511909a9de55f
jenitivecase <jbrussow@gmail.com>
2017-06-05 13:07:23 -0500

    added additional needed params

--- base_simulation_script.R
+++ base_simulation_script.R
@@ -12,11 +12,16 @@ N_groups <- 300
 groupsize_min <- 5
 groupsize_max <- 35
 n_items <- 60
+mean_increase <- 0.5
+yr_corr <- 0.6
+cheat_eff <- 0.5
+n_cheat <- N_groups * 0.05
 
 items <- item_sim(n_items, b_mean = 0, b_sd = 1,
                   a_min = 0.5, a_max = 3)
 
 people <- ability_sim(N_people, theta_mean=0, theta_sd=1,
-                    N_groups, groupsize_min, groupsize_max)
+                    N_groups, groupsize_min, groupsize_max, 
+                    mean_increase, yr_corr, cheat_eff)
 
 dataset <- one_dataset(person_param = people, item_param = items)
