6fec658013fbdbb15e9314e28971e2741388a228
jenitivecase <jbrussow@gmail.com>
2017-06-13 13:35:09 -0500

    working(?) draft. no R2 recovery

--- stan_script.R
+++ stan_script.R
@@ -1,68 +1,62 @@
 stancode_long <- "
 data {
-int<lower=0> n_people;
-int<lower=0> n_items;
-int<lower=0> n_observations;
-int<lower=0, upper=n_people> respondentid[n_observations];
-int<lower=0, upper=n_items> itemid[n_observations];
-int<lower=0, upper=1> response[n_observations];
-int<lower=0, upper=1> group_long[n_observations];
-vector[n_people] group;
+  int<lower=0> n_people;
+  int<lower=0> n_items;
+  int<lower=0> n_observations;
+  int<lower=0> n_groups;
+  int<lower=0, upper=n_people> studentid[n_observations];
+  int<lower=0, upper=n_items> itemid[n_observations];
+  int<lower=0, upper=1> response[n_observations];
+  int<lower=0, upper=1> groupid[n_observations];
 }
 
 parameters {
-vector<lower=0>[n_items] a;
-vector[n_items] b;
-vector[n_people] theta1;
-vector[n_people] theta2;
-real corr;
-real mean_inc;
-real cheat_eff;
-real indiv_inc;
-real<lower=0> sigma2;
-
+  vector<lower=0>[n_items] a;
+  vector[n_items] b;
+  vector[n_people] theta1;
+  real<lower=0, upper=1> corr;
+  real<lower=0> sigma2;
+  real mu2;
 }
 
 transformed parameters {
-vector[n_items] mu;
-vector[n_items] ss_err;
-vector[n_items] ss_reg;
-vector[n_people] mu_theta;
-real R2;
-
-mu_theta = foc_mean*group;
-
-for (j in 1:n_items) {
-mu[j] = beta0 + beta1*DIFpredict[j];
-}
-
-D = mu + sigma2*D_raw;
-
-for (j in 1:n_items) {
-ss_err[j] = pow((D[j]-mu[j]),2);
-ss_reg[j] = pow((mu[j]-mean(D[])),2);
-}
-
-R2 = sum(ss_reg[])/(sum(ss_reg[])+sum(ss_err[]));
+  vector[n_groups] group_inc;
+  vector[n_people] indiv_err;
+  vector[n_people] theta2;
+  # vector[n_items] ss_err;
+  # vector[n_items] ss_reg;
+  # real R2;
+  
+  for (i in 1:n_observations) {
+    theta2[studentid[i]] = corr*theta1[studentid[i]] + group_inc[groupid[i]] + indiv_err[studentid[i]];
+  }
+  
+  
+### how to calculate here?
+#   for (j in 1:n_items) {
+#     ss_err[j] = pow((D[j]-mu[j]),2);
+#     ss_reg[j] = pow((mu[j]-mean(D[])),2);
+#   }
+#   
+#   R2 = sum(ss_reg[])/(sum(ss_reg[])+sum(ss_err[]));
 }
 
 model {
-vector[n_observations] eta;
-
-a ~ lognormal(0, 1);
-b ~ normal(0, 1);
-theta ~ normal(mu_theta, 1);
-D_raw ~ normal(0, 1);
-//  D ~ normal(mu, sigma2);
-foc_mean ~ normal(0, 4);
-beta0 ~ normal(0, 1);
-beta1 ~ normal(0, 1);
-sigma2 ~ normal(0, 10);
-
-for(i in 1:n_observations){
-eta[i] = a[itemid[i]]*(theta[respondentid[i]] - (b[itemid[i]] + D[itemid[i]] * group_long[i]));
-}
-
-response ~ bernoulli_logit(eta);
+  vector[n_observations] eta;
+  
+  a ~ lognormal(0, 1);
+  b ~ normal(0, 1);
+  theta2 ~ normal(mu2, 1);
+  theta1 ~ normal(0, 1);
+  sigma2 ~ normal(0, 10);
+  corr ~ normal(0, 1);
+  group_inc ~ normal(0, 3);
+  indiv_err ~ normal(0, 3);
+  
+  for(i in 1:n_observations){
+    eta[i] = a[itemid[i]]*(theta2[studentid[i]] - (b[itemid[i]]));
+  }
+  
+  response ~ bernoulli_logit(eta);
 }
 "
